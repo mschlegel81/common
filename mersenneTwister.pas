9101e7bdb409f1c701a23de8f8ade61a193482f6
Martin Schlegel <schlegel@itestra.de>
2016-08-11 12:11:21 +0200

    Checkstyle

--- mersenneTwister.pas
+++ mersenneTwister.pas
@@ -41,7 +41,7 @@ begin
         { See Knuth TAOCP Vol2. 3rd ED. P.106 for multiplier.
           in the previous versions, MSBs of the seed affect
           only MSBs of the array mt[].
-          2002/01/09 Modified by Makoto Matsumoto }
+          2002/01/09 modified by Makoto Matsumoto }
       mt[i] := mt[i] and $ffffffff;
         { For >32 Bit machines }
     end;
